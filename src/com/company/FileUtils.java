32b4dd35c8883d365ffd42536fc8a55731cb6d6f
Max <51697067+MacU55@users.noreply.github.com>
2021-04-19 12:09:10 +0300

    changed: 1. Method getUnsortedNumbers(File fileToRead) was changed. Now it makes to extract two types data(ints and chars) from mixed source, and put these two data types into files for numbers and characters. 2. Added method saveChars() to save (if they are) characters into file chars.txt

--- src/com/company/FileUtils.java
+++ src/com/company/FileUtils.java
@@ -1,6 +1,8 @@
 package com.company;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,27 +10,92 @@ import java.util.Scanner;
 
 public class FileUtils {
 
-    //method to get unsorted list of numbers, reading numbers from file-source
+    static File fileToRead;
+    static File fileToSave;
+    private static File fileToSaveChars = new File("./chars.txt");
 
+    static boolean validateFileForReading(String fileNameToRead){
+
+        try {
+            fileToRead = new File(fileNameToRead);
+
+            if (!fileToRead.exists()) {
+                System.out.println( "\"" + fileNameToRead + "\" File is not found. Nothing to sort. Check up path to file with unsorted numbers.");
+                return false;// if file is not exists, then in main() method will be return
+            }
+            if (!fileToRead.canRead()) {
+                System.out.println( "\'" + fileNameToRead + "\' File is impossible to read. Make another readable file with *.txt extension.");
+                return false;
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+    static boolean validateFileForWriting (String fileNameToSaveSortedData){
+
+        try {
+            fileToSave = new File( fileNameToSaveSortedData);
+            if(fileToSave.exists()){
+                System.out.println("File to save sorted numbers is already exists: "+ fileToSave.getAbsolutePath());
+            }
+            else  if (!fileToSave.exists()) {
+                fileToSave = new File("D:\\INTERNSHIP\\JAVA_ROADMAP\\sortingNumbers_3\\results\\sortedNumbers.txt");
+                System.out.println("File \"" + fileNameToSaveSortedData + "\" is not found. Sorted numbers will be saved to new file: " + fileToSave.getAbsolutePath());
+            }
+            else if(!fileToSave.canWrite()){
+                System.out.println("\'" + fileNameToSaveSortedData + "\' file is invalid for writing");
+                return false;
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+    //method to get unsorted list of numbers, also it can read chars from file-source
     public static List<Integer> getUnsortedNumbers(File fileToRead) {
 
         List <Integer> unsortedNumbers = new ArrayList<>();
+        List <String> someChars = new ArrayList<>();
+        int numbersCounter = 0;
+        int charsCounter = 0;
 
         try {
+            FileReader fileReader = new FileReader(fileToRead);
+            BufferedReader reader = new BufferedReader(fileReader);
             Scanner scanner = new Scanner(fileToRead);
-            int i = 0;
-            while (scanner.hasNext()) {
-                unsortedNumbers.add(i++, scanner.nextInt());
+
+            String line = reader.readLine();
+            while (line != null) {
+                String [] splitString = line.split("\\s+");//split line into array of chars
+                for(String g : splitString){//check every char: number or not?
+                    if(g.matches("\\d+(\\d+)?")){// here char is number
+                        unsortedNumbers.add(numbersCounter++, Integer.valueOf(g) );
+                    }
+                    else someChars.add(charsCounter++, g);// here is char
+                }
+                line = reader.readLine();
             }
-        } catch (NumberFormatException t) {
-            System.out.println("check format data in file");
-        } catch (Exception e) {
+            fileReader.close();
+            reader.close();
+            scanner.close();
+        }
+        catch (Exception e) {
             e.printStackTrace();
         }
-
+        if(numbersCounter == 0){
+            System.out.println(" Numbers were not found in file: " + fileToRead.getAbsolutePath());
+        }
+        if(charsCounter > 0){
+            saveChars(someChars, fileToSaveChars);
+            System.out.println("\n Some characters were found and saved in file: " + fileToSaveChars.getAbsolutePath());
+        }
         return unsortedNumbers;
     }
-
     // method to write sorted numbers into file
     public static void saveSortedNumbers(List<Integer> sortedNumbers, File fileToSave) {
 
@@ -37,7 +104,20 @@ public class FileUtils {
                 String s = Integer.toString(sortedNumbers.get(i));
                 fileWriter.write(s);
                 fileWriter.write(System.lineSeparator());
-                // fileWriter.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    //method for saving chars in file, if chars are in file)
+    public static void saveChars(List<String> someChars, File fileToSaveChars) {
+
+        try (FileWriter fileWriter = new FileWriter(fileToSaveChars)) {
+            for (int i = 0; i < someChars.size(); i++) {
+                String s = (someChars.get(i));
+                fileWriter.write(s);
+                fileWriter.write(System.lineSeparator());
+                //fileWriter.close();
             }
         } catch (Exception e) {
             e.printStackTrace();
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,28 +1,30 @@
 package com.company;
-import java.io.*;
 import java.util.List;
 import java.util.Scanner;
 
 
 public class Main {
 
-    private static File fileToRead;
-    private static File fileToSave;
+
     public static Scanner scanner;
 
     public static void main(String [] args) {
 
         scanner = new Scanner(System.in);
-        String fileNameToRead = getFirstParam("Specify path to file with unsorted numbers ", scanner);
-        String fileNameToSaveSortedData = getFileNameToSave(" Specify path and file name to save sorted data", scanner);
+        String fileNameToRead = getFirstParam(" Specify path to file with unsorted numbers ", scanner);
+        String fileNameToSaveSortedData = getFileNameToSave("\n Specify path and file name to save sorted data ", scanner);
 
-        // added checking: if problems with files, app should be exit
-        if (!validateFiles(fileNameToRead, fileNameToSaveSortedData))
+        // checking file for reading: if problems with file, app is to close
+        if (!FileUtils.validateFileForReading(fileNameToRead))
             return;
 
-        List<Integer> unsortedNumbersList = FileUtils.getUnsortedNumbers(fileToRead);
+        // checking file for writing: if problems with file, app is to close
+        if (!FileUtils.validateFileForWriting(fileNameToSaveSortedData))
+            return;
+
+        List<Integer> unsortedNumbersList = FileUtils.getUnsortedNumbers(FileUtils.fileToRead);
         Sorting.selectSortingMethod(unsortedNumbersList);
-        FileUtils.saveSortedNumbers(Sorting.numbersToSave, fileToSave);
+        FileUtils.saveSortedNumbers(Sorting.numbersToSave, FileUtils.fileToSave);
     }
 
     private static String getFirstParam (String messageUserCanSeeInConsole, Scanner scanner) {
@@ -34,41 +36,6 @@ public class Main {
         System.out.println(messageUserCanSeeInConsole);
         return scanner.nextLine() + ".txt";
     }
-
-    private static boolean validateFiles(String fileNameToRead, String fileNameToSaveSortedData){
-
-        try {
-            fileToRead = new File(fileNameToRead);
-
-            if (!fileToRead.exists()) {
-                System.out.println( "\"" + fileNameToRead + "\" File is not found. Nothing to sort. Check up path to file with unsorted numbers.");
-                return false;// if file is not exists, then in main() method will be return
-            }
-            if (!fileToRead.canRead()) {
-                System.out.println( "\'" + fileNameToRead + "\' File impossible to read. Make another readable file with *.txt extension.");
-                return false;
-            }
-
-            fileToSave = new File( fileNameToSaveSortedData);
-
-            if(fileToSave.exists()){
-                System.out.println("File to save sorted numbers is already exists: "+ fileToSave.getAbsolutePath());
-            }
-            else  if (!fileToSave.exists()) {
-                fileToSave = new File("D:\\INTERNSHIP\\JAVA_ROADMAP\\Sorting numbers\\results\\sortedNumbers.txt");
-                System.out.println("File \"" + fileNameToSaveSortedData + "\" is not found. Sorted numbers will be saved to new file: " + fileToSave.getAbsolutePath());
-            }
-            else if(!fileToSave.canWrite()){
-                System.out.println("\'" + fileNameToSaveSortedData + "\' file is invalid for writing");
-                return false;
-            }
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-        return true;
-    }
-
 }
 
 
